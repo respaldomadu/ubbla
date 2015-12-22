0bc07e8f45c30b1925f73f64a7531bf89f865f58
bgirlflaca <barbaraa.conii@gmail.com>
2015-12-22 15:50:01 -0300

    calse configuraciones y configuracion

---
+++ CapaNegocio/ClaseConfiguracion.cs
@@ -0,0 +1,40 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+   public class ClaseConfiguracion
+    {
+        public ClaseConexion objconexion = new ClaseConexion();
+
+        public String ActualizarConfiguracion(int anio, int periodo)
+        {
+            List<ClaseParametros> lst = new List<ClaseParametros>();
+            String Mensaje = "";
+            try
+            {
+                lst.Add(new ClaseParametros("@ANIO", anio));
+                lst.Add(new ClaseParametros("@PERIODO", periodo));
+                lst.Add(new ClaseParametros("@MENSAJE", "", SqlDbType.VarChar, ParameterDirection.Output, 150));
+                objconexion.EjecutarSP("SP_ACTUALIZAR_CONFIGURACIONES", ref lst);
+                Mensaje = lst[3].Valor.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Mensaje;
+        }
+
+        public DataTable Listado()
+        {
+            return objconexion.Listado("SP_LISTAR_CONFIGURACIONES", null);
+        }
+
+    }
+}
