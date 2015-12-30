852fb6134ef5ab1d19d11b80652c3878a75407e5
respaldomadu <m.rios2012@hotmail.com>
2015-12-30 11:31:03 -0300

    ESTE 2

--- PROYECTO_UB/MODULO.Designer.cs
+++ PROYECTO_UB/MODULO.Designer.cs
@@ -29,14 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MODULO));
-            this.CB_DIA = new System.Windows.Forms.ComboBox();
-            this.CB_INICIO = new System.Windows.Forms.ComboBox();
-            this.CB_FIN = new System.Windows.Forms.ComboBox();
-            this.CODIGO = new System.Windows.Forms.Label();
-            this.NOMBRE = new System.Windows.Forms.Label();
-            this.INICIO = new System.Windows.Forms.Label();
-            this.FIN = new System.Windows.Forms.Label();
-            this.TXT_COD = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btn_agregar = new System.Windows.Forms.Button();
             this.btn_actualizar = new System.Windows.Forms.Button();
@@ -46,80 +38,19 @@
             this.label5 = new System.Windows.Forms.Label();
             this.txt_buscar = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TXT_COD = new System.Windows.Forms.TextBox();
+            this.FIN = new System.Windows.Forms.Label();
+            this.INICIO = new System.Windows.Forms.Label();
+            this.NOMBRE = new System.Windows.Forms.Label();
+            this.CODIGO = new System.Windows.Forms.Label();
+            this.CB_FIN = new System.Windows.Forms.ComboBox();
+            this.CB_INICIO = new System.Windows.Forms.ComboBox();
+            this.CB_DIA = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // CB_DIA
-            // 
-            this.CB_DIA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.CB_DIA.FormattingEnabled = true;
-            this.CB_DIA.Location = new System.Drawing.Point(12, 102);
-            this.CB_DIA.Name = "CB_DIA";
-            this.CB_DIA.Size = new System.Drawing.Size(121, 21);
-            this.CB_DIA.TabIndex = 0;
-            // 
-            // CB_INICIO
-            // 
-            this.CB_INICIO.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.CB_INICIO.FormattingEnabled = true;
-            this.CB_INICIO.Location = new System.Drawing.Point(12, 151);
-            this.CB_INICIO.Name = "CB_INICIO";
-            this.CB_INICIO.Size = new System.Drawing.Size(68, 21);
-            this.CB_INICIO.TabIndex = 1;
-            // 
-            // CB_FIN
-            // 
-            this.CB_FIN.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.CB_FIN.FormattingEnabled = true;
-            this.CB_FIN.Location = new System.Drawing.Point(86, 151);
-            this.CB_FIN.Name = "CB_FIN";
-            this.CB_FIN.Size = new System.Drawing.Size(64, 21);
-            this.CB_FIN.TabIndex = 2;
-            // 
-            // CODIGO
-            // 
-            this.CODIGO.AutoSize = true;
-            this.CODIGO.Location = new System.Drawing.Point(13, 39);
-            this.CODIGO.Name = "CODIGO";
-            this.CODIGO.Size = new System.Drawing.Size(49, 13);
-            this.CODIGO.TabIndex = 3;
-            this.CODIGO.Text = "CODIGO";
-            // 
-            // NOMBRE
-            // 
-            this.NOMBRE.AutoSize = true;
-            this.NOMBRE.Location = new System.Drawing.Point(13, 86);
-            this.NOMBRE.Name = "NOMBRE";
-            this.NOMBRE.Size = new System.Drawing.Size(25, 13);
-            this.NOMBRE.TabIndex = 4;
-            this.NOMBRE.Text = "DIA";
-            // 
-            // INICIO
-            // 
-            this.INICIO.AutoSize = true;
-            this.INICIO.Location = new System.Drawing.Point(13, 135);
-            this.INICIO.Name = "INICIO";
-            this.INICIO.Size = new System.Drawing.Size(39, 13);
-            this.INICIO.TabIndex = 5;
-            this.INICIO.Text = "INICIO";
-            // 
-            // FIN
-            // 
-            this.FIN.AutoSize = true;
-            this.FIN.Location = new System.Drawing.Point(87, 135);
-            this.FIN.Name = "FIN";
-            this.FIN.Size = new System.Drawing.Size(24, 13);
-            this.FIN.TabIndex = 6;
-            this.FIN.Text = "FIN";
-            // 
-            // TXT_COD
-            // 
-            this.TXT_COD.Enabled = false;
-            this.TXT_COD.Location = new System.Drawing.Point(13, 56);
-            this.TXT_COD.Name = "TXT_COD";
-            this.TXT_COD.Size = new System.Drawing.Size(100, 20);
-            this.TXT_COD.TabIndex = 7;
             // 
             // groupBox1
             // 
@@ -127,7 +58,7 @@
             this.groupBox1.Controls.Add(this.btn_actualizar);
             this.groupBox1.Controls.Add(this.btn_limpiar);
             this.groupBox1.Controls.Add(this.btn_eliminar);
-            this.groupBox1.Location = new System.Drawing.Point(16, 189);
+            this.groupBox1.Location = new System.Drawing.Point(11, 189);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(358, 60);
             this.groupBox1.TabIndex = 9;
@@ -197,7 +128,7 @@
             this.btn_excel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btn_excel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_excel.Image = ((System.Drawing.Image)(resources.GetObject("btn_excel.Image")));
-            this.btn_excel.Location = new System.Drawing.Point(369, 25);
+            this.btn_excel.Location = new System.Drawing.Point(394, 24);
             this.btn_excel.Name = "btn_excel";
             this.btn_excel.Size = new System.Drawing.Size(32, 26);
             this.btn_excel.TabIndex = 13;
@@ -219,7 +150,7 @@
             this.txt_buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_buscar.Location = new System.Drawing.Point(168, 25);
             this.txt_buscar.Name = "txt_buscar";
-            this.txt_buscar.Size = new System.Drawing.Size(195, 26);
+            this.txt_buscar.Size = new System.Drawing.Size(220, 26);
             this.txt_buscar.TabIndex = 11;
             this.txt_buscar.TextChanged += new System.EventHandler(this.txt_buscar_TextChanged);
             // 
@@ -232,33 +163,120 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(418, 133);
+            this.dataGridView1.Size = new System.Drawing.Size(258, 133);
             this.dataGridView1.TabIndex = 10;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TXT_COD);
+            this.groupBox2.Controls.Add(this.FIN);
+            this.groupBox2.Controls.Add(this.INICIO);
+            this.groupBox2.Controls.Add(this.NOMBRE);
+            this.groupBox2.Controls.Add(this.CODIGO);
+            this.groupBox2.Controls.Add(this.CB_FIN);
+            this.groupBox2.Controls.Add(this.CB_INICIO);
+            this.groupBox2.Controls.Add(this.CB_DIA);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(150, 172);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ingresar Modulos";
+            // 
+            // TXT_COD
+            // 
+            this.TXT_COD.Enabled = false;
+            this.TXT_COD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_COD.Location = new System.Drawing.Point(7, 42);
+            this.TXT_COD.Name = "TXT_COD";
+            this.TXT_COD.Size = new System.Drawing.Size(100, 26);
+            this.TXT_COD.TabIndex = 15;
+            // 
+            // FIN
+            // 
+            this.FIN.AutoSize = true;
+            this.FIN.Location = new System.Drawing.Point(81, 121);
+            this.FIN.Name = "FIN";
+            this.FIN.Size = new System.Drawing.Size(24, 13);
+            this.FIN.TabIndex = 14;
+            this.FIN.Text = "FIN";
+            // 
+            // INICIO
+            // 
+            this.INICIO.AutoSize = true;
+            this.INICIO.Location = new System.Drawing.Point(7, 121);
+            this.INICIO.Name = "INICIO";
+            this.INICIO.Size = new System.Drawing.Size(39, 13);
+            this.INICIO.TabIndex = 13;
+            this.INICIO.Text = "INICIO";
+            // 
+            // NOMBRE
+            // 
+            this.NOMBRE.AutoSize = true;
+            this.NOMBRE.Location = new System.Drawing.Point(7, 73);
+            this.NOMBRE.Name = "NOMBRE";
+            this.NOMBRE.Size = new System.Drawing.Size(25, 13);
+            this.NOMBRE.TabIndex = 12;
+            this.NOMBRE.Text = "DIA";
+            // 
+            // CODIGO
+            // 
+            this.CODIGO.AutoSize = true;
+            this.CODIGO.Location = new System.Drawing.Point(7, 25);
+            this.CODIGO.Name = "CODIGO";
+            this.CODIGO.Size = new System.Drawing.Size(49, 13);
+            this.CODIGO.TabIndex = 11;
+            this.CODIGO.Text = "CODIGO";
+            // 
+            // CB_FIN
+            // 
+            this.CB_FIN.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB_FIN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CB_FIN.FormattingEnabled = true;
+            this.CB_FIN.Location = new System.Drawing.Point(80, 135);
+            this.CB_FIN.Name = "CB_FIN";
+            this.CB_FIN.Size = new System.Drawing.Size(64, 28);
+            this.CB_FIN.TabIndex = 10;
+            // 
+            // CB_INICIO
+            // 
+            this.CB_INICIO.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB_INICIO.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CB_INICIO.FormattingEnabled = true;
+            this.CB_INICIO.Location = new System.Drawing.Point(6, 135);
+            this.CB_INICIO.Name = "CB_INICIO";
+            this.CB_INICIO.Size = new System.Drawing.Size(68, 28);
+            this.CB_INICIO.TabIndex = 9;
+            // 
+            // CB_DIA
+            // 
+            this.CB_DIA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB_DIA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CB_DIA.FormattingEnabled = true;
+            this.CB_DIA.Location = new System.Drawing.Point(6, 89);
+            this.CB_DIA.Name = "CB_DIA";
+            this.CB_DIA.Size = new System.Drawing.Size(121, 28);
+            this.CB_DIA.TabIndex = 8;
             // 
             // MODULO
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 261);
+            this.ClientSize = new System.Drawing.Size(455, 261);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.btn_excel);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txt_buscar);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.TXT_COD);
-            this.Controls.Add(this.FIN);
-            this.Controls.Add(this.INICIO);
-            this.Controls.Add(this.NOMBRE);
-            this.Controls.Add(this.CODIGO);
-            this.Controls.Add(this.CB_FIN);
-            this.Controls.Add(this.CB_INICIO);
-            this.Controls.Add(this.CB_DIA);
             this.Name = "MODULO";
             this.Text = "MODULO";
             this.Load += new System.EventHandler(this.MODULO_Load);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -266,14 +284,6 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox CB_DIA;
-        private System.Windows.Forms.ComboBox CB_INICIO;
-        private System.Windows.Forms.ComboBox CB_FIN;
-        private System.Windows.Forms.Label CODIGO;
-        private System.Windows.Forms.Label NOMBRE;
-        private System.Windows.Forms.Label INICIO;
-        private System.Windows.Forms.Label FIN;
-        private System.Windows.Forms.TextBox TXT_COD;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btn_agregar;
         private System.Windows.Forms.Button btn_actualizar;
@@ -283,5 +293,14 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txt_buscar;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox TXT_COD;
+        private System.Windows.Forms.Label FIN;
+        private System.Windows.Forms.Label INICIO;
+        private System.Windows.Forms.Label NOMBRE;
+        private System.Windows.Forms.Label CODIGO;
+        private System.Windows.Forms.ComboBox CB_FIN;
+        private System.Windows.Forms.ComboBox CB_INICIO;
+        private System.Windows.Forms.ComboBox CB_DIA;
     }
 }
