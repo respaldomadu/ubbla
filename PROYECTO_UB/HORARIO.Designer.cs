767afcdedfb5d9b092894dc762bbeb1cf259f6cb
respaldomadu <m.rios2012@hotmail.com>
2016-01-06 12:26:41 -0300

    HH

--- PROYECTO_UB/HORARIO.Designer.cs
+++ PROYECTO_UB/HORARIO.Designer.cs
@@ -84,9 +84,12 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(17, 80);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(364, 194);
             this.dataGridView1.TabIndex = 55;
