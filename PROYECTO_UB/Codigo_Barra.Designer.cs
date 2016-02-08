6a8e5cfd52b3654ce1253688921fa404c85af191
respaldomadu <m.rios2012@hotmail.com>
2016-02-08 13:16:20 -0300

    esto si bpablo perra rey

--- PROYECTO_UB/Codigo_Barra.Designer.cs
+++ PROYECTO_UB/Codigo_Barra.Designer.cs
@@ -30,21 +30,24 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_cantidad = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txt_cantidad = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 84);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 84);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(500, 224);
             this.dataGridView1.TabIndex = 46;
@@ -65,6 +68,24 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Codigo de Barras";
             // 
+            // txt_cantidad
+            // 
+            this.txt_cantidad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.txt_cantidad.FormattingEnabled = true;
+            this.txt_cantidad.Location = new System.Drawing.Point(309, 30);
+            this.txt_cantidad.Name = "txt_cantidad";
+            this.txt_cantidad.Size = new System.Drawing.Size(121, 21);
+            this.txt_cantidad.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(328, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "N° Copias";
+            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(436, 27);
@@ -78,10 +99,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Bar-Code 39 lesbar", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(217, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 32);
+            this.label1.Size = new System.Drawing.Size(0, 31);
             this.label1.TabIndex = 6;
             // 
             // button1
@@ -102,29 +123,11 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(328, 14);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "N° Copias";
-            // 
-            // txt_cantidad
-            // 
-            this.txt_cantidad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.txt_cantidad.FormattingEnabled = true;
-            this.txt_cantidad.Location = new System.Drawing.Point(309, 30);
-            this.txt_cantidad.Name = "txt_cantidad";
-            this.txt_cantidad.Size = new System.Drawing.Size(121, 21);
-            this.txt_cantidad.TabIndex = 10;
-            // 
             // Codigo_Barra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 324);
+            this.ClientSize = new System.Drawing.Size(538, 324);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Codigo_Barra";
