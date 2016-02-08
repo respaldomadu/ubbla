1be2a36b99d506accf75c37e008b212c14c92f76
bgirlflaca <barbaraa.conii@gmail.com>
2016-02-08 11:53:53 -0300

    KIKOKI

--- PROYECTO_UB/Codigo_Barra.cs
+++ PROYECTO_UB/Codigo_Barra.cs
@@ -28,7 +28,7 @@ namespace PROYECTO_UB
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (this.textBox1.Text =="")
+           if (this.textBox1.Text =="")
                 {
                 MessageBox.Show("Seleccione Planificacion", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 this.textBox1.Focus();
@@ -46,8 +46,8 @@ namespace PROYECTO_UB
                         txt_cantidad.Focus();
                         return;
                     }
-          
             
+           
        
             System.Drawing.Printing.PaperSize Tamaño = default(System.Drawing.Printing.PaperSize);
                 Tamaño = new System.Drawing.Printing.PaperSize("tamaño", 850, 1100);//tamaño de hoja carta
@@ -70,35 +70,197 @@ namespace PROYECTO_UB
 
         private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
         {
+          
             for (int i = 5; i < 6; i++)
             {
+              
 
-                for (int a = 1; a < System.Int32.Parse(txt_cantidad.Text)+1; a++)
+                for ( int a = 1; a < System.Int32.Parse(txt_cantidad.Text)+1; a++)
                 {
 
                     e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black,i*10, a*80);
                     e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 45, 20);
-                    //e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 10, a * 80);
-                    
-                    
-                    
-                    
+   
                     Pen blackPen = new Pen(Color.Black, 1);//
-
-                    //e.Graphics.DrawRectangle(blackPen, 10, 95, 700, 20);
-                    //e.Graphics.DrawLine(blackPen, 80, 80, 80, 80);
-
-
 
 
                     Point point1 = new Point(50, 50);
                     Point point2 = new Point(150, 50);
                     e.Graphics.DrawLine(blackPen, point1, point2);
 
+                    int x = 0;
+                    int y = 0;
+                    int width = 200;
+                    int height = 200;
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+             
+                }
+                for (int s =1; s < System.Int32.Parse(txt_cantidad.Text) + 1; s++)
+                {
+
+                    e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 40, s * 80);
+                    e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 200, 20);
+
+                    Pen blackPen = new Pen(Color.Black, 1);//
 
 
-                    // Rectangle destRect1 = new Rectangle(70, 1, 200, 200);
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(150, 50);
+                    e.Graphics.DrawLine(blackPen, point1, point2);
 
+                    int x = 0;
+                    int y = 0;
+                    int width = 200;
+                    int height = 200;
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+                }
+                for (int s = 1; s < System.Int32.Parse(txt_cantidad.Text) + 1; s++)
+                {
+
+                    e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 70, s * 80);
+                    e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 355, 20);
+
+                    Pen blackPen = new Pen(Color.Black, 1);//
+
+
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(150, 50);
+                    e.Graphics.DrawLine(blackPen, point1, point2);
+
+                    int x = 0;
+                    int y = 0;
+                    int width = 200;
+                    int height = 200;
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+                }
+                for (int s = 1; s < System.Int32.Parse(txt_cantidad.Text) + 1; s++)
+                {
+
+                    e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 100, s * 80);
+                    e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 510, 20);
+
+                    Pen blackPen = new Pen(Color.Black, 1);//
+
+
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(150, 50);
+                    e.Graphics.DrawLine(blackPen, point1, point2);
+
+                    int x = 0;
+                    int y = 0;
+                    int width = 200;
+                    int height = 200;
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+                }
+                for (int s = 1; s < System.Int32.Parse(txt_cantidad.Text) + 1; s++)
+                {
+
+                    e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 130, s * 80);
+                    e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 655, 20);
+
+                    Pen blackPen = new Pen(Color.Black, 1);//
+
+
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(150, 50);
+                    e.Graphics.DrawLine(blackPen, point1, point2);
+
+                    int x = 0;
+                    int y = 0;
+                    int width = 200;
+                    int height = 200;
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+                }
+                //**************************************************************************************************************************************
+                for (int a = 5; a < System.Int32.Parse(txt_cantidad.Text) + 5; a++)
+                {
+
+                    e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 10, a * 80);
+                    e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 45, 360);
+
+                    Pen blackPen = new Pen(Color.Black, 1);//
+
+
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(150, 50);
+                    e.Graphics.DrawLine(blackPen, point1, point2);
+
+                    int x = 0;
+                    int y = 0;
+                    int width = 200;
+                    int height = 200;
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+
+                }
+                for (int s = 5; s < System.Int32.Parse(txt_cantidad.Text) + 5; s++)
+                {
+
+                    e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 40, s * 80);
+                    e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 200, 20);
+
+                    Pen blackPen = new Pen(Color.Black, 1);//
+
+
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(150, 50);
+                    e.Graphics.DrawLine(blackPen, point1, point2);
+
+                    int x = 0;
+                    int y = 0;
+                    int width = 200;
+                    int height = 200;
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+                }
+                for (int s = 5; s < System.Int32.Parse(txt_cantidad.Text) + 5; s++)
+                {
+
+                    e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 70, s * 80);
+                    e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 355, 20);
+
+                    Pen blackPen = new Pen(Color.Black, 1);//
+
+
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(150, 50);
+                    e.Graphics.DrawLine(blackPen, point1, point2);
+
+                    int x = 0;
+                    int y = 0;
+                    int width = 200;
+                    int height = 200;
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+                }
+                for (int s = 5; s < System.Int32.Parse(txt_cantidad.Text) + 5; s++)
+                {
+
+                    e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 100, s * 80);
+                    e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 510, 20);
+
+                    Pen blackPen = new Pen(Color.Black, 1);//
+
+
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(150, 50);
+                    e.Graphics.DrawLine(blackPen, point1, point2);
+
+                    int x = 0;
+                    int y = 0;
+                    int width = 200;
+                    int height = 200;
+                    GraphicsUnit units = GraphicsUnit.Pixel;
+                }
+                for (int s = 5; s < System.Int32.Parse(txt_cantidad.Text) + 5; s++)
+                {
+
+                    e.Graphics.DrawString(this.label1.Text, new Font("Bar-Code 39 lesbar", 26, FontStyle.Regular), Brushes.Black, i * 130, s * 80);
+                    e.Graphics.DrawString(nombre_C, new Font("arial", 11, FontStyle.Regular), Brushes.Black, 655, 20);
+
+                    Pen blackPen = new Pen(Color.Black, 1);//
+
+
+                    Point point1 = new Point(50, 50);
+                    Point point2 = new Point(150, 50);
+                    e.Graphics.DrawLine(blackPen, point1, point2);
 
                     int x = 0;
                     int y = 0;
@@ -122,16 +284,16 @@ namespace PROYECTO_UB
             txt_cantidad.Items.Add("1");
             txt_cantidad.Items.Add("2");
             txt_cantidad.Items.Add("3");
-            txt_cantidad.Items.Add("4");
-            txt_cantidad.Items.Add("5");
-            txt_cantidad.Items.Add("6");
-            txt_cantidad.Items.Add("7");
-            txt_cantidad.Items.Add("8");
-            txt_cantidad.Items.Add("9");
-            txt_cantidad.Items.Add("10");
-            txt_cantidad.Items.Add("11");
-            txt_cantidad.Items.Add("12");
-            txt_cantidad.Items.Add("13");
+            //txt_cantidad.Items.Add("4");
+            //txt_cantidad.Items.Add("5");
+            //txt_cantidad.Items.Add("6");
+            //txt_cantidad.Items.Add("7");
+            //txt_cantidad.Items.Add("8");
+            //txt_cantidad.Items.Add("9");
+            //txt_cantidad.Items.Add("10");
+            //txt_cantidad.Items.Add("11");
+            //txt_cantidad.Items.Add("12");
+            //txt_cantidad.Items.Add("13");
 
             txt_cantidad.SelectedIndex = 0;
 
@@ -145,13 +307,23 @@ namespace PROYECTO_UB
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            if (this.textBox1.Text == "")
+            int i = 0;
+         /*  if (this.textBox1.Text == "")
             {
-                MessageBox.Show("Seleccione Planificacion para convertit ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+              MessageBox.Show("Seleccione Planificacion para convertit ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else {label1.Text = "*" + this.textBox1.Text + "*"; }
-
+           else {label1.Text = "*" + this.textBox1.Text + "*"; }
+           /**********************/
             
+            
+            DataTable dt=new DataTable();
+            
+            for (i = 0; i < dt.Rows.Count; i++)
+            {
+                dt = objPlan.Listado();
+                label1.Text = "*" + dt.Rows[i].ItemArray[0].ToString() + "*";
+              
+            }
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -172,6 +344,11 @@ namespace PROYECTO_UB
                 e.Handled = true;
                 return;
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            global.GridAExcel(dataGridView1);
         }
     }
 }
